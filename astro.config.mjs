e93129228561d0ce45a4fc0ff57cde2b7317035b
DilloABoncio <dilloaboncio@gmail.com>
2026-07-01 10:16:57 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://dilloaboncio.netlify.app',
+  site: 'https://dillo-a-boncio.pages.dev',
   output: 'static',
+  vite: {
+    publicDir: 'public',
+  },
 });
